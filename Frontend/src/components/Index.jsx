475a3f12a7a0c337c47f43186d18eff8d9049b5c
Ján Kapurík <jankokapurik@gmail.com>
2024-05-06 22:38:45 +0200

    Chujovinky

--- Frontend/src/components/Index.jsx
+++ Frontend/src/components/Index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import HallOfFame from "./HallOfFame";
+import Comments from "./Comments";
 
 const Index = () => (
-	<main>
-        Our favourite games: <br/>
-        <ol>
-            <li><NavLink to="/cuberoll/new">Cube roll</NavLink></li>
-            <li><NavLink to="">Other game</NavLink></li>
-        </ol>
+	<main className="flex flex-col align-middle space-y-10">
+       <button className="p4 mt-10 p-1 text-lg rounded-md border-2 border-black hover:text-white hover:bg-black">
+         play
+        </button>
+        <HallOfFame/>
+        <Comments/>
     </main>
 );
 
--- Frontend/src/components/layout/Footer.jsx
+++ Frontend/src/components/layout/Footer.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Footer = () => (
-	<footer className="bg-gray-800 text-white py-4">
+	<footer className="h-16">
 		<div className="container mx-auto text-center">
 			<hr />
 			<span>
-				© 2023 Gamestudio was developped at{" "}
+				© Jan Kapurik <br/> 
+				2023 Gamestudio was developped at{" "}
 				<a href="http://kpi.fei.tuke.sk">KPI FEI TUKE</a>
 			</span>
 		</div>
